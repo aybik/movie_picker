f2e1b03ae75a5519035841a12f5a598b957677c6
duonghaxuyen <duonghaxuyen@gmail.com>
2025-02-03 13:00:19 +0100

    monday morning

--- moviepicker/ml_logic/data.py
+++ moviepicker/ml_logic/data.py
@@ -19,13 +19,24 @@ def get_set_a():
     movies = movies[movies['description'].notnull() & (movies['description'] != '')] # Remove movies without description
 
     movies = movies.rename(columns={"date": "year"})
-    movies["year"] = movies["year"].astype(float).apply(lambda x: str(int(x)) if not pd.isna(x) else "")
+    # movies["year"] = movies["year"].astype(float).apply(lambda x: str(int(x)) if not pd.isna(x) else "")
+    movies["year"] = pd.to_numeric(movies["year"], errors='coerce').astype("Int64")
 
     movies = movies.dropna(subset=['minute']) # Remove NaN values in minute
     movies['minute'] = movies['minute'].astype(int) # Change minute dtype to int
     movies = movies[(movies['minute'] > 40) & (movies['minute'] <= 240)] # Remove short and too long movies
 
-    movies['key'] = movies['name'] + movies['year'].apply(lambda x: f" ({int(x)})" if x!='' else '')
+    movies['key'] = movies['name'] + movies['year'].apply(lambda x: '' if pd.isna(x) else f" ({int(x)})")
+
+    unique_keys = movies['key'].value_counts()
+    filter_a = unique_keys[unique_keys > 1].index
+    filter_a = movies[movies['key'].isin(filter_a)] # Entries not unique
+    filter_a = filter_a[['id','year','rating', 'key']]
+    filter_a[["year", "rating"]] = filter_a[["year", "rating"]].fillna(0)
+    filter_a = filter_a.sort_values(by=["key", "year", "rating", "id"], ascending=[True, False, False, True])
+    filter_result = filter_a.drop_duplicates(subset=["key"], keep="first").drop(columns=["rating", "year"])
+    keys_with_count_1 = unique_keys[unique_keys == 1].index
+    new_movies = movies[(movies['key'].isin(keys_with_count_1)) | (movies['id'].isin(filter_result['id']))]
 
     # Clean up actors df
     actors = actors[actors['role'].notnull() & (actors['role'] != '')]  # Remove actors without role
@@ -70,14 +81,14 @@ def get_set_a():
     )
 
     # Merge into 1 df
-    data = movies \
+    data = new_movies \
         .merge(new_genres, how='left', on='id') \
         .merge(new_actors, how='left', on='id') \
         .merge(languages, how='left', on='id') \
         .merge(new_studios, how='left', on='id') \
         .merge(new_crew, how='left', on='id')
 
-    data[["language", "genre_list"]] = data[["language", "genre_list"]].fillna("")
+    # data[["language", "genre_list"]] = data[["language", "genre_list"]].fillna("")
     data[["actor_list", "studio_list"]] = data[["actor_list", "studio_list"]].applymap(lambda x: x if isinstance(x, list) else [])
 
     return data
