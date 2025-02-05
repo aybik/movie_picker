5c86ca7e0114dbc9127b8e6a3c929785b5c9cdc3
duonghaxuyen <duonghaxuyen@gmail.com>
2025-02-05 14:50:24 +0100

    wed

--- moviepicker/api/fast.py
+++ moviepicker/api/fast.py
@@ -1,15 +1,26 @@
 import pandas as pd
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from moviepicker.ml_logic import archived_preprocessor, model
+from moviepicker.ml_logic import advanced_model
 import pickle
 import os
+import re
+import requests
+import string
+from tensorflow.keras.models import load_model
 
 app = FastAPI()
-app.state.model = pickle.load(open(os.path.join(os.path.dirname(os.path.dirname(__file__)),"models/knn_model.pkl"), "rb"))
-app.state.data = pd.read_csv(os.path.join(os.path.dirname(os.path.dirname(__file__)),"final_set_a.csv")) ###EDIT
-app.state.matrix = pickle.load(open(os.path.join(os.path.dirname(os.path.dirname(__file__)),"models/matrix.pkl"), "rb"))
+parent_dir = os.path.dirname(os.path.dirname(__file__))
 
+# Load pickle files
+app.state.model = pickle.load(open(os.path.join(parent_dir,"models/knn_20.pkl"), "rb"))
+app.state.latent_embeddings = pickle.load(open(os.path.join(parent_dir,"models/latent_embeddings.pkl"), "rb"))
+app.state.vectorizer = pickle.load(open(os.path.join(parent_dir,"models/vectorizer.pkl"), "rb"))
+app.state.encoder_trained = load_model(os.path.join(parent_dir,"models/encoder_model.keras"))
+
+# Load dataframes
+app.state.data = pd.read_pickle(os.path.join(parent_dir,"data_encode.pkl"))
+app.state.full_data = pd.read_pickle(os.path.join(parent_dir,"streamlit.pkl"))
 
 # Allowing all middleware is optional, but good practice for dev purposes
 app.add_middleware(
@@ -20,27 +31,59 @@ app.add_middleware(
     allow_headers=["*"],  # Allows all headers
 )
 
-# http://127.0.0.1:8000/predict?input_name=Inception%20%282010%29&name_column=key_b
-# http://127.0.0.1:8000/predict?input_name=Inception&name_column=name
-
-# http://127.0.0.1:8000/predict?input=Inception%20%282010%29&type=key_b
-# http://127.0.0.1:8000/predict?input=Inception&type=name
-
-@app.get("/predict")
+@app.get("/predict_name")
 def predict(
-        input: str,
-        type: str,
-    ):      # 1
-    """
-    Make a single course prediction.
-    Assumes `pickup_datetime` is provided as a string by the user in "%Y-%m-%d %H:%M:%S" format
-    Assumes `pickup_datetime` implicitly refers to the "US/Eastern" timezone (as any user in New York City would naturally write)
-    """
-    # YOUR CODE HERE
-    result = model.get_similar_movies_knn(app.state.model, app.state.matrix, app.state.data, input, type)
+        input_name: str,
+        n_recommendations: int
+    ):
+    result = advanced_model.get_movie_recommendations(input_name, app.state.data, app.state.model, app.state.latent_embeddings, n_recommendations)
     return result
+
+@app.get("/predict_filter")
+def predict(
+        user_description: str,
+        user_language: str,
+        user_genres: str,
+        n_recommendations: int
+    ):
+    user_genres = list(user_genres)
+    result = advanced_model.recommend_movies_by_details(user_description, user_language, user_genres, app.state.data, app.state.encoder_trained, app.state.model, app.state.vectorizer, n_recommendations)
+    return result
+
+@app.get("/find")
+def find_movies(input_name, dataset_choice):
+    if dataset_choice == "full":
+        df = app.state.full_data
+    elif dataset_choice == "final":
+        df = app.state.data
+    movie_name = input_name.lower()
+    df['name'] = df.name.apply(lambda x: x.lower())
+    same_movies_df = df[df.name == movie_name]
+    return list(same_movies_df.key_b)
+
+
+@app.get("/get_url")
+def get_url(movie):
+    film_id = app.state.full_data[app.state.full_data["key_b"] == movie]["film_id"].iloc[0]
+    return app.state.full_data.loc[app.state.full_data['film_id']==film_id, 'streamlit_url'].iloc[0]
+
+@app.get("/get_image")
+def get_image(movie):
+
+    df = app.state.full_data
+    if movie in df.key_b.values:
+        poster = df.loc[df.key_b == movie, "poster"].iloc[0]
+        return poster
+    else:
+        return None
+
+@app.get("/get_description")
+def get_description(movie):
+    df = app.state.full_data
+    result = df[df["key_b"] == movie]["description"]
+    return result.iloc[0]
+
 
 @app.get("/")
 def root():
-    # YOUR CODE HERE
     return {'greeting': 'Hello'}
